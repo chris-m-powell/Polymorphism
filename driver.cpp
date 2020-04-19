60a1fbb447315f1179a29b24e33788d596e55e9e
Chris Powell <chris.m.powell@protonmail.com>
2020-04-19 10:53:31 -0700

    update

--- driver.cpp
+++ driver.cpp
@@ -3,9 +3,11 @@
 //-------------------------------------------------
   /*
     This program exhibits polymorphic behavior as client() accepts a pointer to 
-    an object of the base class that is type-compatible with pointers to either 
-    derived class. When invoked, the respective implementations of method_a() 
-    and method_b() will be resolved based on the derived-type pointer passed.
+    an object of the base class (i.e., Base<Derived1> or Base<Derived2>) but 
+    is type-compatible with pointers to either derived class (i.e., Derived1 
+    or Derived2); that is, when invoked, the implementation of 
+    method_a() and method_b() is be resolved based on the derived-type pointer 
+    passed.
   */
 template <class T>
 void client(Base<T>* x) { 
@@ -14,10 +16,10 @@ void client(Base<T>* x) {
 }
 //-------------------------------------------------
 int main() {
-  Derived1 D1;
+  Derived1 D1;  //declares on object of type Derived1 which inherits from Base<Derived1> 
   client(&D1);
   
-  Derived2 D2;
+  Derived2 D2;  //declares on object of type Derived2 which inherits from Base<Derived2>
   client(&D2);
 
   return 0;
